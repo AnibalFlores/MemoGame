9f233a86ca08c0c61abcd6a644c2535185783bda
Aníbal H. Flores <floresanibal@yahoo.com.ar>
2018-08-26 14:41:10 -0300

    js added

---
+++ scripts.js
@@ -0,0 +1,9 @@
+var mostrar = function(p){
+    alert('el boton es: ' + p.dataset.value )
+    p.querySelector("#card").className = "opaco";
+    p.querySelector("#ask").className = "oculto";
+    
+};
+
+    var test = function(p){
+        alert('image clicked!' + p)};
